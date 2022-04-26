9232415fe4ba7022b38489707aa963f1c24c29dd
Nathan Jaggers <nathanj8910@gmail.com>
2022-04-26 12:51:42 -0700

    init

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*******************************************************
+PROGRAM NAME - Monty Hall Problem
+
+PROGRAMMER - Nathan Jaggers
+
+DATE - 04/26/22
+
+DESCRIPTION - This program implements the Monty Hall
+              Problem and shows how it is statisctically
+              advantangous to switch your guess during
+              the problem.
+
+*******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
